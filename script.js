2b743a526858825957de04be9bec1c91f8033c42
Dmitriy E <dsanich@saifas.org>
2021-12-15 12:42:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function initFunc() {
-  console.log('init');
+API_KEY = "ST_MzdjYzYwN2EtZTIyNy00MjZkLTk3MGMtYzY3ODg3NjBjNzg0NjM3NzM1OTYyNTY5NjM2MzY5"
+GET_ORDER_URL = "https://app.snipcart.com/api/orders/"
+
+const token = window.location.href.split('https://dsanichsai.github.io/qr-code-gen.github.io/', '');
+
+async function initFunc() {
+  const request = await fetch(`${GET_ORDER_URL}${token}`, {
+      headers: {
+        'Authorization': `Basic ${btoa(API_KEY)}`,
+        'Accept': 'application/json'
+      }
+    });
+  const result = await request.json();
+  console.log(result);
 };
